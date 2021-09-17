43bac79464db45b1c820c7f85b55b9da5b4206a2
Attah David <jocheattahdavid@gmail.com>
2021-09-17 18:08:44 +0100

    wip sidebar save main screen animation

--- navigation/CustomDrawer.js
+++ navigation/CustomDrawer.js
@@ -1,39 +1,159 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {View} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+} from "@react-navigation/drawer";
 
-import {MainLayout} from '../screens';
-import {COLORS} from '../constants';
+import { MainLayout } from "../screens";
+import {
+  COLORS,
+  constants,
+  dummyData,
+  FONTS,
+  icons,
+  SIZES,
+} from "../constants";
 
 const Drawer = createDrawerNavigator();
+
+const CustomDrawerItem = ({ label, icon }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        height: 40,
+        marginBottom: SIZES.base,
+        alignItems: "center",
+        paddingLeft: SIZES.radius,
+        borderRadius: SIZES.base,
+        //backgroundColor
+      }}
+      // onPress={()=>}
+    >
+      <Image
+        source={icon}
+        // alt={label}
+        style={{ width: 20, height: 20, tintColor: COLORS.white }}
+      />
+      <Text style={{ marginLeft: 15, color: COLORS.white, ...FONTS.h3 }}>
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const CustomDrawerContent = ({ navigation }) => {
+  return (
+    <DrawerContentScrollView
+      scrollEnabled={true}
+      contentContainerStyle={{ flex: 1 }}
+    >
+      <View style={{ flex: 1, paddingHorizontal: SIZES.radius }}>
+        {/* close */}
+        <View style={{ alignItems: "flex-start", justifyContent: "center" }}>
+          <TouchableOpacity
+            style={{ alignItems: "center", justifyContent: "center" }}
+            onPress={() => navigation.closeDrawer()}
+          >
+            <Image
+              source={icons.cross}
+              style={{ height: 35, width: 35, tintColor: COLORS.white }}
+            />
+          </TouchableOpacity>
+        </View>
+        {/* profile  */}
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: SIZES.radius,
+          }}
+          onPress={() => console.log("Profile")}
+        >
+          <Image
+            source={dummyData.myProfile?.profile_image}
+            style={{ height: 50, width: 50, borderRadius: SIZES.radius }}
+          />
+          <View style={{ marginLeft: SIZES.radius }}>
+            <Text style={{ color: COLORS.white, ...FONTS.h3 }}>
+              {dummyData.myProfile?.name}{" "}
+            </Text>
+            <Text style={{ color: COLORS.white, ...FONTS.body4 }}>
+              View your profile
+            </Text>
+          </View>
+        </TouchableOpacity>
+        {/* draweritems */}
+        <View style={{ flex: 1, marginTop: SIZES.padding }}>
+          <CustomDrawerItem label={constants.screens.home} icon={icons.home} />
+          <CustomDrawerItem
+            label={constants.screens.my_wallet}
+            icon={icons.wallet}
+          />
+          <CustomDrawerItem
+            label={constants.screens.notification}
+            icon={icons.notification}
+          />
+          <CustomDrawerItem
+            label={constants.screens.favourite}
+            icon={icons.favourite}
+          />
+          {/* line divider */}
+          <View
+            style={{
+              height: 1,
+              marginVertical: SIZES.radius,
+              marginLeft: SIZES.radius,
+              backgroundColor: COLORS.lightGray1,
+            }}
+          ></View>
+          <CustomDrawerItem label="Track Your Order" icon={icons.location} />
+          <CustomDrawerItem label="Coupons" icon={icons.coupon} />
+          <CustomDrawerItem label="Settings" icon={icons.setting} />
+          <CustomDrawerItem label="Invite a friend" icon={icons.profile} />
+          <CustomDrawerItem label="Help Center" icon={icons.help} />
+        </View>
+        <View style={{marginBottom:SIZES.padding}}>
+          <CustomDrawerItem label="Logout" icon={icons.logout} />
+        </View>
+      </View>
+    </DrawerContentScrollView>
+  );
+};
 
 const CustomDrawer = () => {
   return (
     <View
       style={{
-        flex: 1,
+        flex: 1, 
         backgroundColor: COLORS.primary,
-      }}>
+      }}
+    >
       <Drawer.Navigator
         drawerType="slide"
         overlayColor="transparent"
         drawerStyle={{
           flex: 1,
-          width: '65%',
+          width: "65%",
           paddingRight: 20,
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         }}
         sceneContainerStyle={{
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         }}
-        initialRouteName="MainLayout">
+        initialRouteName="MainLayout"
+        drawerContent={(props) => {
+          return <CustomDrawerContent navigation={props.navigation} />;
+        }}
+      >
         <Drawer.Screen name="MainLayout">
-          {props => <MainLayout {...props} />}
+          {(props) => <MainLayout {...props} />}
         </Drawer.Screen>
       </Drawer.Navigator>
     </View>
   );
 };
 
-export default CustomDrawer; 
+export default CustomDrawer;
